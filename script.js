b3fe0f0b5a3194539e6208abfcc6585bda3549d4
Anna Dolejší <dolejsianna@email.cz>
2022-08-02 09:29:16 +0200

    rozklad na prvočísla

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JavaScript funguje!");
+console.log("JavaScript funguje!")
+let n = prompt('Které číslo mám rozložit na prvočísla?')
+let a = 2
+while (a <= n) {
+    if (n % a == 0){
+        document.write(a + ', ');
+        n = n / a;
+    }
+    else {
+        a += 1;
+    }
+    
+}
